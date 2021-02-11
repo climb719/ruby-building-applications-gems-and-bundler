81a504d1a8f0f3167a8ab0252510560ebb571333
climb719 <mary.oldweiler@gmail.com>
2021-02-11 11:28:57 -0500

    commit tree 85b7ab6fc3eb17ee8b6df11bcc01fccb4ecf6896

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,8 @@
 # Set up Bundler here following the instructions in the README
+
+require 'bundler/setup'
+Bundler.require(:default, :development) 
+
+#first requiring 'bundler/setup' If we don't us this line
+#our app won't know to use bundler to install our gems and Gemfile becomes pointless
+#must be in this order,  app needs to know the order in which to load files
